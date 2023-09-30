8f12bca1a94a6837847d09f6a02b9df0de0119c6
Seema-spec <79435657+Seema-spec@users.noreply.github.com>
2023-09-30 16:23:40 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import VideoListing from './Components/VideoListing';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <VideoListing/>
+    </div>
+  );
+}
+
+export default App;
